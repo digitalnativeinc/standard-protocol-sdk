a407c601d4750bc6ea294942ebcca10bf299a672
billjhlee <billjhlee@gmail.com>
2021-10-01 18:24:27 +0900

    Rinkeby Test Contract Addresses

--- src/types/ProtocolMap.ts
+++ src/types/ProtocolMap.ts
@@ -4,5 +4,6 @@ export type ProtocolMap = {
   FACTORY_ADDRESS: AddressMap;
   ROUTER_ADDRESS: AddressMap;
   MASTER_POOL_ADDRESS?: AddressMap;
+  DIVIDEND_POOL_ADDRESS?: AddressMap;
   INIT_CODE_HASH: { FACTORY: AddressMap };
 };
